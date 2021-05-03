734796f1a6e7c570cec90b839a9305036590ed4a
nellyXinwei <nelsonalbajr@yahoo.com>
2021-05-03 15:31:30 +0800

    finished ## 1.4 Create API Controllers (CRUD functions for Todos)

--- src/types/todo.ts
+++ src/types/todo.ts
@@ -1,6 +1,5 @@
 import { Document } from "mongoose"
 
-
 //Here, we have a Todo interface that extends the Document type provided by mongoose. We will be using it later to interact with MongoDB. That said, we can now define how a Todo model should look.
 export interface ITodo extends Document {
   name: string
