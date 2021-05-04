d9f871adf0670942ac78c5a9c023f287b2d2f026
nellyXinwei <nelsonalbajr@yahoo.com>
2021-05-05 00:23:43 +0800

    added mongodb to env, init on client side with react and ts setting up

--- server/src/app.ts
+++ server/src/app.ts
@@ -10,9 +10,9 @@ const PORT:string|number = process.env.PORT||4000
 app.use(cors());
 app.use(todoRoutes);
 
-const uri:string = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@clustertodo.raz9g.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`;
-const options = {useNewUrlParser:true, useUnifiedTopology:true};
-mongoose.set("useFindAndModify",false);
+const uri: string = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@clustertodo.raz9g.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`
+const options = { useNewUrlParser: true, useUnifiedTopology: true }
+mongoose.set('useFindAndModify', false)
 
 mongoose
 .connect(uri,options)
